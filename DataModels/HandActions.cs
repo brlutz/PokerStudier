320c44050b9974b7f3b031fc17f6109286b10ddb
Ben Lutz <ben.ryan.lutz@gmail.com>
2019-09-22 20:21:02 -0400

    Inital commit for hero action detection

---
+++ DataModels/HandActions.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+public static class HandActions
+{
+    
+    /* 
+    public static readonly string FoldedBeforeFlop = "FoldedBeforeFlop";
+    public static readonly string CheckBeforeFlop = "CheckBeforeFlop";
+    public static readonly string CallBeforeFlop = "CallBeforeFlop";
+    public static readonly string LimpedBeforeFlop = "LimpedBeforeFlop";
+    public static readonly string RaisedBeforeFlop = "RaisedBeforeFlop";
+
+
+    public static readonly string FoldedToBetOnFlop = "FoldedToBetOnFlop";
+    public static readonly string CalledBetOnFlop = "CalledBetOnFlop";
+    public static readonly string BetOnFlop = "BetOnFlop";
+    public static readonly string RaisedOnFlop = "RaisedOnFlop";
+
+    public static readonly string FoldedToBetOnTurn = "FoldedToBetOnTurn";
+    public static readonly string CalledBetOnTurn = "CalledBetOnTurn";
+        public static readonly string BetOnTurn = "BetOnTurn";
+    public static readonly string RaisedOnTurn = "RaisedOnTurn";
+
+    public static readonly string FoldedToBetOnRiver = "FoldedToBetOnRiver";
+    public static readonly string CalledBetOnRiver = "CalledBetOnRiver";
+        public static readonly string BetOnRiver = "BetOnRiver";
+    public static readonly string RaisedOnRiver = "RaisedOnRiver";
+     */
+
+    public static readonly string Fold = "Fold";
+    public static readonly string Check = "Check";
+    public static readonly string Call = "Call";
+    public static readonly string Limped = "Limped";
+
+    
+    public static readonly string Bet = "Bet";
+    public static readonly string Raised = "Raised";
+
+    public static readonly string BeforeFlop = "BeforeFlop";
+    public static readonly string Flop = "Flop";
+    public static readonly string Turn = "Turn";
+
+    public static readonly string River = "River";
+}
