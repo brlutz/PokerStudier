29bdedc1e2387490600654d0c9508f868d447bb4
Ben Lutz <blutz@billtrust.com>
2019-10-07 20:46:30 -0400

    added service to clean up controller with addition hands

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,95 +13,28 @@ namespace PokerStudier1.Controllers
     {
         public IActionResult Index(string position)
         {
-
-
-            return View(GetWholeRangeAnaylsisModelGetter(position));
+            
+            ViewModelService s = new ViewModelService();
+            return View(s.GetWholeRangeAnaylsisModelGetter(position));
         }
 
         [Route("/Hand/{hand}", Name = "Hand")]
         public IActionResult Hand(string hand)
         {
-            return View(GetHandAnaylsisModelGetter(hand));
+            ViewModelService s = new ViewModelService();
+            return View(s.GetHandAnaylsisModelGetter(hand));
             // return View(ModelGetter(position));
         }
 
         [Route("/Hands", Name = "Hands")]
         public IActionResult Hands(string hand)
         {
-            return View(GetHandsAnaylsisModelGetter());
+            ViewModelService s = new ViewModelService();
+            return View(s.GetHandsAnaylsisModelGetter());
             // return View(ModelGetter(position));
         }
 
-        private HandOverviewViewModel GetHandAnaylsisModelGetter(string hand)
-        {
-            List<string> textFiles = GetHandHistoryFiles();
 
-            PokerParser p = new PokerParser();
-            foreach (string file in textFiles)
-            {
-                p.ReadInFile(file);
-            }
-
-            Filter f = new Filter(null, hand);
-            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
-            List<string> actionOptions = new List<string>();
-
-            return new HandOverviewViewModel(p.HandHistories, f);
-        }
-
-        private HandsOverviewViewModel GetHandsAnaylsisModelGetter()
-        {
-            List<string> textFiles = GetHandHistoryFiles();
-            PokerParser p = new PokerParser();
-            foreach (string file in textFiles)
-            {
-                p.ReadInFile(file);
-            }
-
-            Filter f = new Filter(null, null, null, "Losses" );
-            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
-            List<string> actionOptions = new List<string>();
-
-            return new HandsOverviewViewModel(p.HandHistories, f, new PaginationSettings() {PageSize = 100});
-        }
-
-        private List<string> GetHandHistoryFiles()
-        {
-              List<string> textFiles = new List<string>() { "HHs/HH1.txt", "HHs/HH2.txt", "HHs/HH3.txt", "HHs/HH4.txt", "HHs/HH5.txt" };
-
-              return textFiles;
-        }
-
-        private ResultsViewModel GetWholeRangeAnaylsisModelGetter(string position)
-        {
-            List<string> textFiles = GetHandHistoryFiles();
-
-            PokerParser p = new PokerParser();
-            foreach (string file in textFiles)
-            {
-                p.ReadInFile(file);
-            }
-            Filter f = new Filter(position, null);
-            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
-            List<string> actionOptions = new List<string>();
-            foreach(HandHistory hh in p.HandHistories)
-            {
-                actionOptions.AddRange(hh.Actions);
-                actionOptions = actionOptions.Distinct().ToList();
-            }
-            List<string> sortedActionOptions = new List<string>();
-            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("BeforeFlop")).ToList());
-            sortedActionOptions.AddRange(actionOptions.Where(x => !x.Contains("Before") && x.Contains("Flop")).ToList());
-            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("Turn")).ToList());
-            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("River")).ToList());
-
-
-
-            f.ActionOptions = sortedActionOptions;
-
-
-            return new ResultsViewModel(a.GetResults(), a.HUDStats, f);
-        }
 
         public IActionResult Privacy()
         {
---
+++ Services/ViewModelService.cs
@@ -0,0 +1,85 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using PokerStudier1.Models;
+
+namespace PokerStudier
+{
+    public class ViewModelService
+    {
+        public HandsOverviewViewModel GetHandsAnaylsisModelGetter()
+        {
+            List<string> textFiles = GetHandHistoryFiles();
+            PokerParser p = new PokerParser();
+            foreach (string file in textFiles)
+            {
+                p.ReadInFile(file);
+            }
+
+            Filter f = new Filter(null, null, null, "Losses");
+            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
+            List<string> actionOptions = new List<string>();
+
+            return new HandsOverviewViewModel(p.HandHistories, f, new PaginationSettings() { PageSize = 100 });
+        }
+
+        public List<string> GetHandHistoryFiles()
+        {
+            List<string> textFiles = new List<string>() { "HHs/HH1.txt", "HHs/HH2.txt", "HHs/HH3.txt", "HHs/HH4.txt", "HHs/HH5.txt", "HHs/HH6.txt" };
+
+            return textFiles;
+        }
+        public ResultsViewModel GetWholeRangeAnaylsisModelGetter(string position)
+        {
+            List<string> textFiles = GetHandHistoryFiles();
+
+            PokerParser p = new PokerParser();
+            foreach (string file in textFiles)
+            {
+                p.ReadInFile(file);
+            }
+            Filter f = new Filter(position, null);
+            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
+            List<string> actionOptions = new List<string>();
+            foreach (HandHistory hh in p.HandHistories)
+            {
+                actionOptions.AddRange(hh.Actions);
+                actionOptions = actionOptions.Distinct().ToList();
+            }
+            List<string> sortedActionOptions = new List<string>();
+            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("BeforeFlop")).ToList());
+            sortedActionOptions.AddRange(actionOptions.Where(x => !x.Contains("Before") && x.Contains("Flop")).ToList());
+            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("Turn")).ToList());
+            sortedActionOptions.AddRange(actionOptions.Where(x => x.Contains("River")).ToList());
+
+
+
+            f.ActionOptions = sortedActionOptions;
+
+
+            return new ResultsViewModel(a.GetResults(), a.HUDStats, f);
+        }
+
+        public HandOverviewViewModel GetHandAnaylsisModelGetter(string hand)
+        {
+            List<string> textFiles = GetHandHistoryFiles();
+
+            PokerParser p = new PokerParser();
+            foreach (string file in textFiles)
+            {
+                p.ReadInFile(file);
+            }
+
+            Filter f = new Filter(null, hand);
+            PokerAnalyser a = new PokerAnalyser(p.HandHistories, f);
+            List<string> actionOptions = new List<string>();
+
+            return new HandOverviewViewModel(p.HandHistories, f);
+        }
+
+
+    }
+}
